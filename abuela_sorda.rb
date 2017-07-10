b64a55d44ff5a820f449403eda546467ed08f640
djpapeles <tiburonaxo@gmail.com>
2017-07-10 13:04:10 +0200

    Add files via upload

--- abuela_sorda.rb
+++ abuela_sorda.rb
@@ -1,5 +1,7 @@
 puts 'Dime, que querias?'
 decir = gets.chomp
+contador = 0
+
 while decir != decir.upcase
     puts 'QUE?, ¡HABLA MAS FUERTE!'
     decir = gets.chomp
@@ -7,7 +9,23 @@ end
 
 while decir != 'ADIOS'
     if decir == decir.upcase
-        puts 'NO, NO DESDE ' + rand(1938..1960).to_s
+        puts 'NO, NO DESDE ' + rand(1938..1960).to_s + ' QUE YO RECUERDE'
+    else
+        puts 'QUE?, ¡HABLA MAS FUERTE!'
+    end
+    decir = gets.chomp
+end
+
+while contador < 3
+    if decir == 'ADIOS'
+        puts 'TODAVIA NO VIENE LA VECINA'
+        contador = contador + 1
+    else
+        puts 'CLARO, PASAME EL MONEDERO'
+    end
+    
+    if decir == decir.upcase
+        puts  'MAS O MENOS DESDE EL OTRO DIA'
     else
         puts 'QUE?, ¡HABLA MAS FUERTE!'
     end
---
+++ iteradores.rb
@@ -0,0 +1,8 @@
+lenguajes = ['Ingles', 'Aleman', 'Ruby']
+
+lenguajes.each do |leng|
+    puts '¡Me gusta ' + leng + '!'
+    puts '¿A ti?'
+end
+puts '¡Y vamos a escuchar esto para C++!'
+puts '...'
